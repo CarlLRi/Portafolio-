5391bcc3a89a41bd49377ffeabec3678519c9c67
Denford20 <cleveyr@ucenfotec.ac.cr>
2024-08-16 17:36:39 -0600

    cambios proyecto

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -7,7 +7,7 @@ langSwitchBtn.addEventListener("click", () => {
         translateToEnglish();
     } else {
         document.documentElement.lang = "es";
-        langSwitchBtn.textContent = "Cambiar a Inglés";
+        langSwitchBtn.textContent = "Switch to English";
         translateToSpanish();
     }
 });
@@ -16,10 +16,14 @@ function translateToEnglish() {
     document.querySelector("#inicio h1").textContent = "Transforming data into intelligence";
     document.querySelector("#inicio h3").textContent = "Data analyst";
     document.querySelector("#perfil h2").textContent = "Professional profile";
-    document.querySelector("#perfil p").textContent = "Data Analyst with experience in managing performance indicators and applying business rules to financial information. Proficient in automation tools (SQL, Power BI, R, database administration), data mining, and programming languages (Java, C#, Python).";
-    document.querySelector("#proyectos h2").textContent = "Projects";   
-    document.querySelector("#proyectos p").textContent = "Here you can see some of my featured projects.";
-    document.querySelector("#contacto h2").textContent = "Contact";
+    document.querySelector("#perfil p").textContent = "Data Analyst with experience in managing performance indicators and applying business rules to financial information. Proficient in automation tools (SQL, Power BI, R, database administration), data mining, and programming languages (Java, C#, Python)."; 
+    document.querySelector("#perfil1 h2").textContent = "Certifications";
+    document.querySelector("#perfil1 p").textContent = "HTML, Scrum, R, Python";
+    document.querySelector("#perfil2 h2").textContent = "Experience";
+    document.querySelector("#perfil2 p").textContent = "Scrum master team leader, Hardware technical support, process automation, create workflows, manage service desk.";
+    document.querySelector("#proyectos h2").textContent = "Projects";  
+    document.querySelector("#proyectos p").textContent = "Here you can see some of my featured projects in Html-C#-Java.";
+    document.querySelector("#contacto h2").textContent = "You can contact me";
     document.querySelector("#contacto label[for='nombre']").textContent = "Name:";
     document.querySelector("#contacto label[for='correo']").textContent = "Email:";
     document.querySelector("#contacto label[for='telefono']").textContent = "Phone:";
@@ -27,7 +31,7 @@ function translateToEnglish() {
     document.querySelector("footer p").textContent = " 2024 Carl Levey Rìos. Professional profile.";
     document.querySelector("footer p:nth-child(2)").textContent = "Email: cleveyr@ucenfotec.ac.cr";
     document.querySelector("footer p:nth-child(3)").textContent = "Phone: +506 87 57 12 94";
-    document.querySelector("footer a").textContent = "Download Resume";
+    document.querySelector("footer a").textContent = "Download Resume to Spanish";
  
 }
 
@@ -36,9 +40,13 @@ function translateToSpanish() {
     document.querySelector("#inicio h3").textContent = "Analista de datos";
     document.querySelector("#perfil h2").textContent = "Perfil profesional";
     document.querySelector("#perfil p").textContent = "Analista de datos, con  manejo de  indicadores de gestión, aplicando reglas de negocio a información financiera. Además cuento con el conocimiento  de herramientas de automatización,  (sql, PowerBi, R, administración de bases de datos),  minería de datos y  lenguajes de programación ( java, C#  y Python).";
+    document.querySelector("#perfil1 h2").textContent = "Certificaciones";
+    document.querySelector("#perfil1 p").textContent = "HTML, Scrum, R, Python";
+    document.querySelector("#perfil2 h2").textContent = "Experiencia";
+    document.querySelector("#perfil2 p").textContent = "Líder de equipo scrum master, Soporte técnico de hardware, automatización de procesos, crear flujos de trabajo, administrar mesa de servicio.";
     document.querySelector("#proyectos h2").textContent = "Proyectos";
-    document.querySelector("#proyectos p").textContent = "Aquí puedes ver algunos de mis proyectos destacados.";
-    document.querySelector("#contacto h2").textContent = "Contacto";
+    document.querySelector("#proyectos p").textContent = "Aquí puedes ver algunos de mis proyectos destacados en Html-C#-Java.";
+    document.querySelector("#contacto h2").textContent = "Me puedes contactar";
     document.querySelector("#contacto label[for='nombre']").textContent = "Nombre:";
     document.querySelector("#contacto label[for='correo']").textContent = "Correo Electrónico:";
     document.querySelector("#contacto label[for='telefono']").textContent = "Teléfono:";
@@ -46,7 +54,7 @@ function translateToSpanish() {
     document.querySelector("footer p").textContent = "2024 Carl Levey Rìos. Perfil Profesional.";
     document.querySelector("footer p:nth-child(2)").textContent = "Correo: cleveyr@ucenfotec.ac.cr";
     document.querySelector("footer p:nth-child(3)").textContent = "Teléfono: +506 87 57 12 94";
-    document.querySelector("footer a").textContent = "Descargar Currículum";
+    document.querySelector("footer a").textContent = "Download Resume to English";
    
 }
 let currentSlide = 0;
@@ -67,3 +75,4 @@ function moveSlide(direction) {
     const slideWidth = slides[0].clientWidth;
     container.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
 }
+
